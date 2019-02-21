1f001399ed986657b8143821746b05cf5117e877
Jon Lynch <jon.lynch@coxautoinc.com>
2019-02-21 10:40:21 -0600

    added initial client files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    message: 'Teachie Awards | A work in progress.'
+  };
+
+  componentDidMount() {
+      // setInterval(this.hello, 250);
+  }
+  // Spring Security is implemented.
+  // hello = () => {
+  //   fetch('/api/hello')
+  //     .then(response => response.text())
+  //     .then(message => {
+  //         this.setState({message: message});
+  //     });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">{this.state.message}</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
