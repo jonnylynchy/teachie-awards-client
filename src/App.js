b552709856de357094b41edbe46b36685de0fc4c
Jon Lynch <jonnylynchy@gmail.com>
2019-02-22 12:39:13 -0600

    added some additional layout to home/landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Row, Col, Nav, NavItem, NavLink, Card, CardTitle, CardText, Button } from 'reactstrap';
 
 import API from './utils/API';
 import './App.css';
+import award from './images/temp-award-2.png';
+// import circle from './images/left-circle-half.svg';
+// import hex from './images/right-hex.svg';
 
 class App extends Component {
   state = {
@@ -24,6 +27,9 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <div className="circle"></div>
+        <div className="hex"></div>
+        {/* NAV */}
         <div className="d-flex flex-row-reverse mr-5">
           <Nav>
             <NavItem>
@@ -33,24 +39,54 @@ class App extends Component {
               <NavLink href="#">About</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Awards</NavLink>
+              <NavLink href="#">Events</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Vote</NavLink>
+              <NavLink href="#">Winners</NavLink>
             </NavItem>
           </Nav>
         </div>
-        <div className="container hero">
-          <div className="row">
-            <div className="col">
+        {/* HERO */}
+        <Container className="hero">
+          <Row className="row">
+            <div className="col-3">
+              <div className="mt-5 pt-5">
+                <img className="mt-5" src={award} alt="Teachie Awards" />
+              </div>
+            </div>
+            <div className="col-9">
               <div className="mt-5 pt-5">
                 <h1 className="mt-5 display-2">Teachie Awards</h1>
                 <p className="lead mt-4 ml-3">The Teachie Awards give students, faculty and communities the ability to recognize the educators among us who give their time to empower kids to have better and brighter futures.</p>
-                <p className="mt-4 ml-3"><small>{this.state.serverMessage}</small></p>
+                <p className="mt-4 ml-3"><small><b>Server Message: </b>{this.state.serverMessage}</small></p>
               </div>
             </div>
-          </div>
-        </div>
+          </Row>
+        </Container>
+        {/* CARDS */}
+        <Container>
+          <Row>
+            <Col sm="12">
+              <div className="my-5 py-5"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="6">
+              <Card body>
+                <CardTitle><h3>Fellow Faculty &amp; Students</h3></CardTitle>
+                <CardText>Register to nominate and vote for your favorite teacher!</CardText>
+                <Button color="primary">Register</Button>
+              </Card>
+            </Col>
+            <Col sm="6">
+              <Card body>
+                <CardTitle><h3>Teachers and Educators</h3></CardTitle>
+                <CardText>Interested in participating in Teachies Register to be rewarded by your peers and students.</CardText>
+                <Button color="primary">Register</Button>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </React.Fragment>
     );
   }
